e18abf0a46f4a535502bf08741bf4833096b5ef2
[GXmax16xd] <[GXmax16@gmail.com]>
2026-01-14 09:49:52 +0100

    Primera prueba

---
+++ src/test/java/com/example/sgeapi/SgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sgeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
