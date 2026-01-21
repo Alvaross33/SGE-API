e5d98c911d2e718812d5c4bd9d62ac3484e4429d
Alvaro Sierra <alvaritosierra1@gmail.com>
2026-01-21 08:54:29 +0100

    segundo push de Alvaro

---
+++ src/main/java/com/example/sgeapi/repository/EmpleadoRepository.java
@@ -0,0 +1,9 @@
+package com.example.sgeapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.sgeapi.model.Empleado;
+
+@Repository
+public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
+}
---
+++ src/main/java/com/example/sgeapi/service/EmpleadoService.java
@@ -0,0 +1,39 @@
+package com.example.sgeapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.sgeapi.dto.EmpleadoDTO;
+import com.example.sgeapi.model.Empleado;
+import com.example.sgeapi.repository.EmpleadoRepository;
+import java.util.List;
+
+@Service
+public class EmpleadoService {
+
+    @Autowired
+    private FabricaEmpleadoService fabricaEmpleadoService;
+
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
+    public EmpleadoDTO save(EmpleadoDTO empleadoDTO) {
+        Empleado empleado = fabricaEmpleadoService.createEmpleado(empleadoDTO);
+        Empleado empleadoGuardado = empleadoRepository.save(empleado);
+        return fabricaEmpleadoService.createEmpleadoDTO(empleadoGuardado);
+    }
+
+    public List<EmpleadoDTO> findAll() {
+        List<Empleado> listaEmpleados = empleadoRepository.findAll();
+        return fabricaEmpleadoService.crearEmpleadosDTO(listaEmpleados);
+    }
+
+    public EmpleadoDTO findById(Integer id) {
+        return empleadoRepository.findById(id)
+                .map(fabricaEmpleadoService::createEmpleadoDTO)
+                .orElse(null);
+    }
+
+    public void deleteById(Integer id) {
+        empleadoRepository.deleteById(id);
+    }
+}
